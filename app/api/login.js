54f5bde57669977d3fe46cbdfba97ab3a1f84784
om salunke <omsaunke343@gmail.com>
2024-08-16 21:00:11 +0530

    setup comit

---
+++ app/api/login.js
@@ -0,0 +1,7 @@
+export default async function POST(req){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ utils/connect.js
@@ -0,0 +1,11 @@
+
+import mongoose from "mongoose";
+
+export const connectToDB = async () => {
+  const url = process.env.DB_URL;
+  if (mongoose.connection.readyState === 1) {
+    return mongoose.connection.asPromise();
+  } else {
+    mongoose.connect(url);
+  }
+};
